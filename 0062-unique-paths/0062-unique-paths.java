a256e7a62c1e67a09c584f30c483237fe2b34a9f
Bhanu Sri Appana <23P31A1269@acet.ac.in>
2025-12-14 16:09:15 +0530

    Time: 0 ms (100%), Space: 41.9 MB (57.57%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        long result = 1;
+        int totalMoves = m + n - 2;
+        int downMoves = Math.min(m - 1, n - 1);
+        for (int i = 1; i <= downMoves; i++) {
+            result = result * (totalMoves - downMoves + i) / i;
+        }
+        return (int) result;
+    }
+}

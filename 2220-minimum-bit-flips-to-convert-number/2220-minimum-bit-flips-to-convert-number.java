15eca5eedc9896d140def5f564f1fbd349753c7c
Bhanu Sri Appana <23P31A1269@acet.ac.in>
2026-03-06 11:19:47 +0530

    Time: 0 ms (100%), Space: 42.1 MB (85.7%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int XOR= start ^ goal;
+        int count=0;
+        while(XOR!=0){
+            if((XOR&1)==1){
+                count++;
+            }
+            XOR=XOR>>1;
+        }
+        return count;
+    }
+}

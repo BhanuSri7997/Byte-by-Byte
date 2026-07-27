9377ab5686bbb12a10f0978e8f557b69c2fe755e
Bhanu Sri Appana <23P31A1269@acet.ac.in>
2026-07-27 10:33:58 +0530

    Time: 1 ms (100%), Space: 118.7 MB (78.89%) - LeetHub

---
+++ 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxValidPairSum(int[] nums, int k) {
+
+        int best = nums[0];
+        int ans = 0;
+
+        for (int j = k; j < nums.length; j++) {
+
+            best = Math.max(best, nums[j - k]);
+
+            ans = Math.max(ans, best + nums[j]);
+        }
+
+        return ans;
+    }
+}
